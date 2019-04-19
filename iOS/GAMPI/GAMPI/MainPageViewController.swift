7545ca9677d86f9d20f9f47ad99c8ef513ae0f39
Dennis Lin <txl429@case.edu>
2019-04-19 02:46:23 -0400

    Added Single Player View Controller

---
+++ iOS/GAMPI/GAMPI/MainPageViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MainPageViewController.swift
+//  GAMPI
+//
+//  Created by Dennis Lin on 4/19/19.
+//  Copyright © 2019 TungHo Lin. All rights reserved.
+//
+
+import UIKit
+
+class MainPageViewController: UIViewController {
+
+    @IBOutlet weak var singleplayer_btn: UIButton!
+    @IBOutlet weak var multiplayer_btn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
