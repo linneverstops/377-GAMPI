b41fc19042b11b514f2169444b012476080b5120
Dennis Lin <txl429@case.edu>
2019-04-22 02:19:13 -0400

    Gameboard in place, created gamepageViewController

---
+++ iOS/GAMPI/GAMPI/GamePageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GamePageViewController.swift
+//  GAMPI
+//
+//  Created by Dennis Lin on 4/22/19.
+//  Copyright © 2019 TungHo Lin. All rights reserved.
+//
+
+import UIKit
+
+class GamePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
